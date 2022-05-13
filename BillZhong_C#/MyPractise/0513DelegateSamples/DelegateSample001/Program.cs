7b8593018faeee2d116c8c24e9c9489dcf517fda
Bowen <b10710080@g.chu.edu.tw>
2022-05-13 13:51:42 +0800

    1

--- BillZhong_C#/MyPractise/0513DelegateSamples/DelegateSample001/Program.cs
+++ BillZhong_C#/MyPractise/0513DelegateSamples/DelegateSample001/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace DelegateSample001
 {
+    public delegate void SomeAction(string message);
     internal class Program
     {
+        static void ShowText(string msg)
+        {
+            Console.WriteLine($"ShowText{msg}");
+        }
+        static void ShowmMssage(string str)
+        {
+            Console.WriteLine($"ShowText{str}");
+        }
         static void Main(string[] args)
         {
+            SomeAction action1=new SomeAction(ShowText);
+            action1 += ShowmMssage;
+
+            SomeAction action2 = ShowText;
+            action1.Invoke("第一個");
+
+            action2("第二個");
+
+            Console.ReadLine();
         }
     }
+
 }
