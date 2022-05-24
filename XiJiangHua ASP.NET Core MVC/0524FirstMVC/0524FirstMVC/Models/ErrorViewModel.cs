ec317f2c885ba80d7aaf72d149aa096a70039ca3
Bowen <b10710080@g.chu.edu.tw>
2022-05-24 12:19:46 +0800

    apple ok

---
+++ XiJiangHua ASP.NET Core MVC/0524FirstMVC/0524FirstMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _0524FirstMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
