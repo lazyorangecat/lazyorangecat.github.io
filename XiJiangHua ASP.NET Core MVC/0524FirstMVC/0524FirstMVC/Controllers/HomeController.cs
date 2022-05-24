7c56f304d709450cad266b301f577a4bc6487777
Bowen <b10710080@g.chu.edu.tw>
2022-05-24 15:51:35 +0800

    0524MVC

--- XiJiangHua ASP.NET Core MVC/0524FirstMVC/0524FirstMVC/Controllers/HomeController.cs
+++ XiJiangHua ASP.NET Core MVC/0524FirstMVC/0524FirstMVC/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace _0524FirstMVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger)               //HomeController:母雞、控制狂
         {
             _logger = logger;
         }
---
+++ XiJiangHua ASP.NET Core MVC/0524FirstMVC/0524FirstMVC/Models/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace _0524FirstMVC.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brief { get;set;}
+        public string Photo { get; set; }
+    }
+}
