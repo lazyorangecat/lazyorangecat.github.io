f2d2535c8feadd8f65dcc91dd7e8dc70f049e18e
Bowen <b10710080@g.chu.edu.tw>
2022-06-02 18:01:11 +0800

    骨架

--- BillZhong_C#/MyPractise/0602ShapeSamples/ShapeLibraryTests/Helpers/ShapeHelperTests.cs
+++ BillZhong_C#/MyPractise/0602ShapeSamples/ShapeLibraryTests/Helpers/ShapeHelperTests.cs
@@ -1,49 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ShapeLibrary.Helpers;
-using ShapeLibrary.Shapes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShapeLibrary.Helpers.Tests
+namespace ShapeLibraryTests.Helpers
 {
-
-    [TestClass()]
-    public class ShapeHelperTests
+    internal class ShapeHelperTests
     {
-        [DataRow(2,4,8)]
-        [DataTestMethod()]
-        public void ComputeAreaTest(double w,double h,double expected)
-        {
-            Rectangle input = new Rectangle()
-            {
-                Width = w,
-                Height = h,
-            };
-            var actual =  input.ComputeArea();
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod()]
-        public void ComputeAreaTest1()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void ComputePerimeterTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void ComputePerimeterTest1()
-        {
-            Assert.Fail();
-        }
-
-
     }
 }
